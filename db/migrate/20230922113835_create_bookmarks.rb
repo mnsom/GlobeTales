8b3a47b9f272cf6526a0c0ba40eaa321b548a549
Mana Misawa <mnmisawa04@gmail.com>
2023-09-25 18:16:06 +0900

    Update models and created seed

--- db/migrate/20230922113835_create_bookmarks.rb
+++ db/migrate/20230922113835_create_bookmarks.rb
@@ -3,7 +3,6 @@ class CreateBookmarks < ActiveRecord::Migration[7.0]
     create_table :bookmarks do |t|
       t.string :name
       t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
