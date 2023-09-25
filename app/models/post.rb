2921f965b8bb56da549d41fd5ab734e8cd7ab67e
Mana Misawa <mnmisawa04@gmail.com>
2023-09-25 13:43:02 +0900

    Updated validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   has_many_attached :photos, dependent: :destroy
 
   validates :title, :description, :location, :category, presence: true
+  validates :title, :description, uniqueness: true
+  validates :description, length: { minimum: 10 }
 end
