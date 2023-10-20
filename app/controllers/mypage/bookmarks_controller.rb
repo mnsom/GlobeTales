773dfec2ea7fcb3b0b47f1cd7911c23607f50f15
Mana Misawa <mnmisawa04@gmail.com>
2023-10-20 22:07:37 +0900

    added created bookmark action

--- app/controllers/mypage/bookmarks_controller.rb
+++ app/controllers/mypage/bookmarks_controller.rb
@@ -4,7 +4,23 @@ class Mypage::BookmarksController < ApplicationController
     @bookmarks = Bookmark.all
   end
 
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.user = current_user
+    if @bookmark.save
+      render json: { success: true }
+    else
+      render json: { success: false, errors: @bookmark.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def show
     @bookmark = Bookmark.find(params[:id])
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:name)
   end
 end
